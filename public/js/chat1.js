eaf52abab72e6e7238afd33d53ea28bcb9b05a43
Danllyo Victor <danllyo.correa@gmail.com>
2026-09-03 10:31:56 -0400

    b

--- public/js/chat1.js
+++ public/js/chat1.js
@@ -27,7 +27,7 @@ async function buscarMensagens(){
 
 buscarMensagens();
 
-const socket = new WebSocket("https://testechat-xi.vercel.app");
+const socket = new WebSocket("ws://testechat-xi.vercel.app");
 
 socket.onopen = () => {
     console.log('conectando com o protocolo');
